e2e14308c51647dd7c50b407fe6861b3640604d8
Robert Main <lenswipe@gmail.com>
2015-09-25 01:04:18 +0100

    Document MySQL Class
    Write docblock documentation for the MySQL class. Ref #10

--- src/adapters/MySQLAdapter.php
+++ src/adapters/MySQLAdapter.php
@@ -1,18 +1,94 @@
 <?php
+
+/**
+ * This file is part of Uniform Cache.
+ * 
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * 
+ * 
+ */
+
 namespace UniformCache\adapters;
-use Exception;
 require_once('Adapter.interface.php');
+
+/**
+ * MySQL
+ *
+ * MySQL Class - This class is used by the MySQL adapter to actually connect to and query MySQL.
+ * 
+ * @author Stewart Atkins
+ * @package UniformCache
+ * @copyright Stewart Atkins
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * 
+ */
 class MySQL_DB {
-	private $user;
-	private $pass;
-	private $host;
-	private $port;
-	private $db;
-	private $friendlyname;
-	private static $links = Array();
-	private $lastError = "";
-	private $insertID = 0;
 	
+	/**
+	 * The username to connect to MySQL with
+	 * @var string
+	 */
+	private $user;
+
+	/**
+	 * The password to connect to MySQL with
+	 * @var string
+	 */
+	private $pass;
+
+	/**
+	 * The hostname of the MySQL server
+	 * @var string
+	 */
+	private $host;
+
+	/**
+	 * The port the MySQL server is running on (defaults to 3306)
+	 * @var int
+	 */
+	private $port;
+
+	/**
+	 * The database to connect to
+	 * @var string
+	 */
+	private $db;
+
+	/**
+	 * An optional name to give the connection to MySQL
+	 * @var string
+	 */
+	private $friendlyname;
+
+	/**
+	 * An array of connection resource link identifiers for the connection
+	 * @var array
+	 */
+	private static $links = Array();
+
+	/**
+	 * The last error to be raised by MySQL
+	 * @var string
+	 */
+	private $lastError = "";
+
+	/**
+	 * The primary key ID of the last record inserted
+	 * @var integer
+	 */
+	private $insertID = 0;
+
+	/**
+	 * Constructs the MySQL adapter
+	 * @param string $hostname The hostname of the MySQL server
+	 * @param int $port The port the MySQL server is running on (defaults to 3306)
+	 * @param string $username The username to connect to MySQL with
+	 * @param string $password The password to connect to MySQL with
+	 * @param string $dbname The name of the database to connect to
+	 * @param string $fname A friendly name to give the connection (if you wish)
+	 */
 	public function __construct($hostname,$port,$username,$password,$dbname,$fname = ""){
 		$this->user = $username;
 		$this->pass = $password;
@@ -22,10 +98,23 @@ class MySQL_DB {
 		$this->friendlyname = $fname;
 	}
 	
+	/**
+	 * Constructs a key used to retrieve link resources
+	 *
+	 * The array (`self::$links`) is keyed using hostname, port, username with each element seperated by a douple pipe. This method generates a key that can be used to retrieve the current MySQL resource link
+	 * 
+	 * @return string A string used to key the `self::$links` array of MySQL resource links.
+	 */
 	private function getLinkKey(){
 		return $this->host."||".$this->port."||".$this->user;
 	}
 	
+	/**
+	 * Returns a single link resource keyed from `self::$links`
+	 *
+	 * This method is used to facilitate connection pooling as it means that the class can support multiple connections to multiple servers simultaneously, but only ever opens one connection per server
+	 * @return resource A link resource representing the MySQL connection
+	 */
 	private function getLink(){
 		if(!isset(self::$links[$this->getLinkKey()])){	
 			$this->createLink();
@@ -33,10 +122,23 @@ class MySQL_DB {
 		return self::$links[$this->getLinkKey()];
 	}
 	
+	/**
+	 * Escape Text
+	 *
+	 * Escapes any text passed in and enables it to be safely passed to MySQL without fear of SQL injection attacks
+	 * 
+	 * @param  string $text An untrusted string potentially containing malicious code
+	 * @return string       A safe, escaped string, ready to be passed to MySQL for storage
+	 */
 	public function escape($text){
 		return mysqli_real_escape_string($this->getLink(),$text);
 	}
 	
+	/**
+	 * Create Link
+	 *
+	 * Creates and stores a MySQL link resource in `self::$links` to facilitate connection pooling
+	 */
 	private function createLink(){
 		$link = @mysqli_connect($this->host,$this->user,$this->pass,"",$this->port);
 		if(mysqli_connect_error()){
@@ -46,6 +148,11 @@ class MySQL_DB {
 		mysqli_set_charset($link,"utf8");
 	}
 	
+	/**
+	 * Execute a query
+	 * @param  string $query An SQL query
+	 * @return \UniformCache\adapters\MySQL_Result An object of type \UniformCache\adapters\MySQL_Result
+	 */
 	public function query($query){
 		$constatus = mysqli_select_db($this->getLink(),$this->db);
 		if(!$constatus){
@@ -92,20 +199,60 @@ class MySQL_DB {
 		}
 	}
 	
+	/**
+	 * Return last error
+	 *
+	 * Returns the last MySQL error to occur. Returns an empty string if no error ocurred.
+	 * @return string The last MySQL error to occur. Empty string if none.
+	 */
 	public function getError(){
 		return $this->lastError;
 	}
 	
+	/**
+	 * Return the ID of the last inserted record
+	 * @return int The primary key value of the last inserted record
+	 */
 	public function insertID(){
 		return $this->insertID;
 	}
 }
-	
+
+/**
+ * MySQL Result
+ *
+ * MySQL result encapsulation class
+ * 
+ * @author Stewart Atkins
+ * @package UniformCache
+ * @copyright Stewart Atkins
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * 
+ */	
 class MySQL_Result{
+
+	/**
+	 * MySQLi prepared statement
+	 * @var object A MySQLi statement object
+	 */
 	private $statement;
+
+	/**
+	 * Query results
+	 * @var mixed The data returned by the query
+	 */
 	public $results;
+
+	/**
+	 * Turns query buffering on and off
+	 * @var boolean
+	 */
 	private $buffered = false;
 	
+	/**
+	 * Constructs the MySQL result class
+	 * @param object $stmt A MySQLi statment
+	 */
 	public function __construct($stmt){
 		$this->statement = $stmt;
 		$fields = mysqli_fetch_fields(mysqli_stmt_result_metadata($this->statement));
@@ -123,8 +270,11 @@ class MySQL_Result{
 		
 	}
 	
-	//get next result record as an associative array
-	//returns false if no more results, or null on error
+	/**
+	 * Get next result record as an associative array
+	 * 
+	 * @return array|boolean|null Returns false if no more results, or null if there was an error.
+	 */
 	public function fetch_assoc(){			
 		$ret = @mysqli_stmt_fetch($this->statement);
 		if($ret){
@@ -136,6 +286,12 @@ class MySQL_Result{
 		return $ret;
 	}
 	
+	/**
+	 * Returns the query results automatically in the most appropriate format
+	 * @param  string $keycol 
+	 * @param  string $valcol 
+	 * @return mixed  The results of the query
+	 */
 	public function fetch_all($keycol = NULL, $valcol = NULL){
 		$results = Array();
 		
@@ -150,8 +306,12 @@ class MySQL_Result{
 		return $results;
 	}
 	
-	//returns number of rows in result set
-	//WARNING: will cause result set to be buffered
+	/**
+	 * Returns the number of rows in the result produced by execution of the query.
+	 *
+	 * **WARNING: ** This will cause the result set fo be buffered.
+	 * @return int The number of rows of data returned by the query
+	 */
 	public function num_rows(){
 		if(!$this->buffered)
 			$this->buffer();
@@ -167,9 +327,37 @@ class MySQL_Result{
 	}
 }
 
+/**
+ * MySQL Adapter
+ *
+ * The MySQL adapter for Uniform Cache
+ * 
+ * @author Robert Main
+ * @package UniformCache
+ * @copyright Robert Main
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * 
+ */
 class MySQLAdapter implements Adapter{
+
+	/**
+	 * The settings array provided to the adapter by the adapter manager
+	 * @var array
+	 */
 	private $config;
+	
+	/**
+	 * An instance of the \UniformCache\adapters\MySQL_DB class
+	 * @var \UniformCache\adapters\MySQL_DB
+	 */
 	private $db;
+	
+	/**
+	 * Constructs the MySQL adapter
+	 *
+	 * Sets up the MySQL adapter by creating the nesecary table (if it doesn't already exist)
+	 * @param array $settings An array of adapter settings
+	 */
 	public function __construct($settings){
 		$this->config = $settings;
 		$this->db = new MySQL_DB($this->config['hostname'], $this->config['port'], $this->config['username'], $this->config['password'], $this->config['database']);
@@ -177,6 +365,16 @@ class MySQLAdapter implements Adapter{
 			$this->db->query("CREATE TABLE IF NOT EXISTS `" . $this->config['table'] . "` (`key` varchar(255) NOT NULL,`value` longtext,`expiresAt` varchar(255) NOT NULL, PRIMARY KEY (`key`));");
 		}
 	}
+
+	/**
+	 *
+	 * Method stub for `get` method in other adapters
+	 * 
+	 * The third argument to this method provides the required data, in the event that this is not available in the cache already(basically, a read through cache).
+	 * 
+	 * @param  mixed $key The key to be used to retrieve the requested data from the cache
+	 * @return boolean Boolean false is always returned since we never have any data stored to return
+	 */
 	public function get($key){
 		$result = $this->db->query("SELECT * FROM " . $this->config['database'] . "." . $this->config['table'] . " WHERE `key` = ? ", $key)->fetch_assoc();
 		if(($result['expiresAt'] <= time()) && ($result['expiresAt'] != 0)){
@@ -187,6 +385,17 @@ class MySQLAdapter implements Adapter{
 			return json_decode($result['value'], true);
 		}
 	}
+	
+	/**
+	 *
+	 * Save an item to the cache.
+	 * 
+	 * Save an item to the cache for the time specificed by the `$ttl` parameter
+	 * 
+	 * @param mixed $key The key used to store the cache data
+	 * @param mixed $value The cache data
+	 * @param int $ttl The time `$value` should be persisted in the cache for (in seconds).
+	 */
 	public function set($key, $value, $ttl=0){
 		$value = json_encode($value);
 		if($ttl != 0){
@@ -194,15 +403,44 @@ class MySQLAdapter implements Adapter{
 		}
 		$this->db->query("INSERT INTO " . $this->config['database'] . '.' . $this->config['table'] . " (`key`, `value`, `expiresAt`) VALUES(?, ?, ?) ON DUPLICATE KEY UPDATE `value` = VALUES(`value`), `expiresAt` = VALUES(`expiresAt`)", $key, $value, $ttl);
 	}
+
+	/**
+	 *
+	 * Remove one item
+	 *
+	 * Removes an item (specified by `$key`) from the cache
+	 * 
+	 * @param mixed $key The key of the data you wish to remove from the cache
+	 */
 	public function delete($key){
 		$this->db->query("DELETE FROM " . $this->config['database'] . '.' . $this->config['table'] . " WHERE `key` = ?", $key);
 	}
+
+	/**
+	 * Purge the cache
+	 *
+	 * Purges the cache completely. On shared resources such as APC or shared memory - a prefix is used when adding items to the cache. This same prefix is then used to avoid collisions when purging(for some reason other users of shared servers tend not to like having their cache forcibly purged by another user...)
+	 */
 	public function purge(){
 		$this->db->query("TRUNCATE " . $this->config['table']);
 	}
+
+	/**
+	 * Get Adaper Priorty
+	 *
+	 * Returns the adapter priorty. Certain adapters take priority over others. For example, the DummyAdapter has the highest priorty(1) because it is always available. Higher priority adapters are examined first to determine if they are instanciable.
+	 * @return int The priroty of the current adapter. A lower number has a higher priorty.
+	 */
 	public static function getPriority(){
 		return 3;
 	}
+
+	/**
+	 * Check If This Adapter Is Usable
+	 *
+	 * Check to see if the adapter is able to be instanciated. Some adapters turn this on and off based on certain conditions. For example, the MySQL adapter might check to see if MySQL is installed and/or enabled and set this to `false` if not. This would prevent UniformCache from trying to instanciate the MySQL adapter.
+	 * @return  boolean Value representing whether or not the adapter can be instanciated
+	 */
 	public static function usable(){
 		return true;
 	}
